a8a828e0e38dd37e936e3c8f063ec86e197fd0ab
ollie-claridge <ollie.claridge@gmail.com>
2024-01-26 19:32:47 +0000

    api setup

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const mainImage = document.querySelector("#poke-con img");
 
-fetch(`https://pokeapi.co/api/v2/pokemon/35`).then(.then(res=>res.json()))
+fetch(`https://pokeapi.co/api/v2/pokemon/35`)
+.then(res=>res.json())
+.then(json=> {
+    console.log(json)
+    console.log(json.sprites.other);
+
+})
+.catch((err) => console.log(err));
+
+ // API connected to var
