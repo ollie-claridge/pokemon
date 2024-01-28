c62a1ba683c4e395d0e7d8b27144959f802d14b1
ollie-claridge <ollie.claridge@gmail.com>
2024-01-28 22:08:06 +0000

    Everything is functional now

--- script.js
+++ script.js
@@ -1,55 +1,71 @@
 const mainImage = document.querySelector("#poke-con img");
 
-const nameText = document.getElementById("name")
-const speciesText = document.getElementById("species")
-const weightText = document.getElementById("weight")
-const heightText = document.getElementById("height")
+const nameText = document.getElementById("name");
+const speciesText = document.getElementById("species");
+const weightText = document.getElementById("weight");
+const heightText = document.getElementById("height");
 const abilitiesList = document.getElementById("abilities-list");
 const getPokemon = document.getElementById("get-poke-button");
 
 //utilaty functions
-function setImgSrc(imgEl, imgSrc){
-    imgEl.setAttribute("src", imgSrc);
+
+function setImgSrc(imgEl, imgSrc) {
+
+  imgEl.setAttribute("src", imgSrc);
+
 }
 
-function setElementText(el, text){
-    el.innerText = text;
+function setElementText(el, text) {
+    
+  el.innerText = text;
 }
 
-function setAbilities(abilitiesArray){
-    let hmtlString =  ``;
-    abilitiesArray.forEach((item) => {
-        hmtlString += `<li>${item.ability.name}</li>`});
-        abilitiesList.innerHTML = hmtlString;
+function setAbilities(abilitiesArray) {
+  let hmtlString = ``;
+  abilitiesArray.forEach((item) => {
+    hmtlString += `<li>${item.ability.name}</li>`;
+  });
+
+  abilitiesList.innerHTML = hmtlString;
+
+}
+
+
+
+function createPokemonProfile(pokemon) {
+    mainImage.setAttribute("src", pokemon.sprites.other.home.front_default); // Corrected from json to pokemon
+    setElementText(nameText, `NAME: ${pokemon.name}`);
+    setElementText(speciesText, `SPECIES: ${pokemon.species.name}`);
+    setElementText(weightText, `WEIGHT: ${pokemon.weight}`);
+    setElementText(heightText, `HEIGHT: ${pokemon.height}`);
+    setAbilities(pokemon.abilities);
 }
 
 const randomPokeIndex = Math.floor(Math.random() * 1000 + 1);
 
-// function createPokemonProfile(pokemon){
-//     mainImage.setAttribute("src", json.sprites.other.home.front_default)
-//     setElementText(nameText, `NAME: ${pokemon.name}`);
-//     setElementText(speciesText, `SPECIES: ${pokemon.species.name}`);
-//     setElementText(weightText, `WEIGHT: ${pokemon.weight}`);
-//     setElementText(heightText,`HEIGHT: ${pokemon.height}`);
-//     setAbilities(pokemon.abilities)
-// }
 
 fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokeIndex}`)
-.then(res=>res.json())
-.then(json=> {
-    console.log(json)
-    //asigning api call to a value; 
-    const pokemon = json;  
-    mainImage.setAttribute("src", json.sprites.other.home.front_default)
-    setElementText(nameText, `NAME: ${pokemon.name}`);
-    setElementText(speciesText, `SPECIES: ${pokemon.species.name}`);
-    setElementText(weightText, `WEIGHT: ${pokemon.weight}`);
-    setElementText(heightText,`HEIGHT: ${pokemon.height}`);
-    setAbilities(pokemon.abilities)
-})
-.catch((err) => console.log(err));
+  .then((res) => res.json())
+  .then((json) => {
+    console.log(json);
+    //asigning api call to a value;
+    const pokemon = json;
+    createPokemonProfile(pokemon)
+  })
+  .catch((err) => console.log(err));
 
- // API connected to var 
+// API connected to var
 
-
+getPokemon.addEventListener("click", function () {
+    
+    const randomPokeIndex = Math.floor(Math.random() * 1000 + 1);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokeIndex}`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        //asigning api call to a value;
+        const pokemon = json;
+        createPokemonProfile(pokemon)
+      })
+      .catch((err) => console.log(err));});
 
